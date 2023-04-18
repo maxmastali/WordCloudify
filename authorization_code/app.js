962a1077e8eded6c31ca27b9c4491419b83e8c95
Maxwell Mastali <maxmastali@gmail.com>
2023-04-18 18:39:29 -0400

    fixed redirect link

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -15,7 +15,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = '51e166b2971b4a9e9ad999e24f45c76e'; // Your client id
 var client_secret = process.env.spotifyDeveloperSecret; // Your secret
-var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
+var redirect_uri = `${process.env.PORT}/callback/`; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
