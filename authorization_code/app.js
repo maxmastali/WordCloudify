d0d58c94ed21c93278b333b053deb5b5f4b081ea
Maxwell Mastali <maxmastali@gmail.com>
2023-04-24 17:23:14 -0400

    update permission request

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -48,8 +48,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email user-top-read';
-  //user-top-read
+  var scope = 'user-top-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
